869fab18b29a162234f978089e9a3d7415285c34
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2019-07-04 21:34:54 +0700

    fixed

--- Circle_and_Radius/src/circle_and_radius/FindingCircle.java
+++ Circle_and_Radius/src/circle_and_radius/FindingCircle.java
@@ -9,20 +9,23 @@ import java.util.Scanner;
 public class FindingCircle {
 	public static void main(String[] args) {
 		int min_index=0;
+		int area_choice;
 		double radius;
 		double diff, diff2;
+		final int NUMBER_OF_CIRCLE = 100;
 		
 		List<Circle> list = new ArrayList<Circle>();
 		Scanner keyboard = new Scanner(System.in);
 		
-		for(int i=0; i<100; i++) {
+		for(int i=0; i<NUMBER_OF_CIRCLE; i++) {
 			list.add(new Circle());
 		}
 		
 		System.out.println("Input the area of the circle you want to find: ");
-		int area_choice = keyboard.nextInt();
-		radius = Math.sqrt(area_choice/Circle.PI);
+		area_choice = keyboard.nextInt();
 		keyboard.close();
+
+		radius = Math.sqrt(area_choice/Circle.PI);
 				
 		Collections.sort(list, new Comparator<Circle>(){
 			public int compare(Circle o1, Circle o2) {
