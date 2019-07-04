191d2f2ac3b5331d8c57834313d12259cff598f1
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2019-07-04 21:27:59 +0700

    fixed

--- Circle_and_Radius/src/circle_and_radius/Circle.java
+++ Circle_and_Radius/src/circle_and_radius/Circle.java
@@ -4,15 +4,13 @@ import java.util.Random;
 
 public class Circle {
 	int radius;
-	double area;
 	static double PI = 3.1416;
 	
 	public Circle() {
 		radius = new Random().nextInt(100)+1;
-		area = radius*radius*PI;
 	}
 	
 	public double getArea() {
-		return this.area;
+		return (radius*radius*PI);
 	}
 }
--- Circle_and_Radius/src/circle_and_radius/FindingCircle.java
+++ Circle_and_Radius/src/circle_and_radius/FindingCircle.java
@@ -9,17 +9,19 @@ import java.util.Scanner;
 public class FindingCircle {
 	public static void main(String[] args) {
 		int min_index=0;
-		double min_area;
+		double radius;
+		double diff, diff2;
+		
 		List<Circle> list = new ArrayList<Circle>();
 		Scanner keyboard = new Scanner(System.in);
 		
 		for(int i=0; i<100; i++) {
 			list.add(new Circle());
 		}
-		min_area= list.get(0).area;
-
+		
 		System.out.println("Input the area of the circle you want to find: ");
 		int area_choice = keyboard.nextInt();
+		radius = Math.sqrt(area_choice/Circle.PI);
 		keyboard.close();
 				
 		Collections.sort(list, new Comparator<Circle>(){
@@ -28,17 +30,20 @@ public class FindingCircle {
 			}
 		});
 		
+		diff = Math.abs(list.get(0).radius-radius);
+		
 		for(int i=1; i<list.size(); i++) {
-			if(Math.abs(list.get(i).area- area_choice) < Math.abs(min_area - area_choice)) {
+			diff2 = Math.abs(list.get(i).radius - radius);
+			if(diff2 < diff) {
 				min_index = i;
-				min_area = list.get(i).area;
+				diff = diff2;
 			}
 		}
 		
-		System.out.println("The circle you need to find has index: "+min_index+" and area is: "+min_area);
+		System.out.println("The circle you need to find has index: "+min_index+" and area is: "+list.get(min_index).getArea());
 		System.out.println();
 		System.out.println("$$$$$$ CIRCLE LIST SORTED BY RADIUS $$$$$$");
 		for(int i=0; i<list.size(); i++)
-			System.out.println("Id: "+i+" - Radius: "+list.get(i).radius+" - Area: "+list.get(i).area);		
+			System.out.println("Id: "+i+" - Radius: "+list.get(i).radius+" - Area: "+list.get(i).getArea());		
 	}
 }
