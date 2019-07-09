dab5dc0660b6de733b41fe7aa9f21299e2d264cb
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2019-07-09 15:24:23 +0700

    fix 1 for loop

--- Circle_and_Radius/src/circle_and_radius/FindingCircle.java
+++ Circle_and_Radius/src/circle_and_radius/FindingCircle.java
@@ -14,6 +14,7 @@ public class FindingCircle {
 		double diff, diff2;
 		final int NUMBER_OF_CIRCLE = 100;
 		
+		List<Integer> index_list = new ArrayList<Integer>();
 		List<Circle> list = new ArrayList<Circle>();
 		Scanner keyboard = new Scanner(System.in);
 		
@@ -22,9 +23,8 @@ public class FindingCircle {
 		}
 		
 		System.out.println("Input the area of the circle you want to find: ");
-		area_choice = keyboard.nextInt();
+		area_choice = keyboard.nextInt();		
 		keyboard.close();
-
 		radius = Math.sqrt(area_choice/Circle.PI);
 				
 		Collections.sort(list, new Comparator<Circle>(){
@@ -35,28 +35,26 @@ public class FindingCircle {
 		
 		diff = Math.abs(list.get(0).radius-radius);
 		
-		for(int i=1; i<list.size(); i++) {
+		for(int i=0; i<list.size(); i++) {
 			diff2 = Math.abs(list.get(i).radius - radius);
 			if(diff2 < diff) {
 				min_index = i;
 				diff = diff2;
+				index_list = new ArrayList<Integer>();
+				index_list.add(i);
 			}
+			else if(diff2 == diff)
+				index_list.add(i);
 		}
-		
-//		System.out.println("The circle you need to find has index: "+min_index+" and area is: "+list.get(min_index).getArea());
-		
+				
 		System.out.println("The circle you need to find has index: ");
-		for(int i=0; i<list.size(); i++) {
-			if(list.get(i).getArea()==list.get(min_index).getArea()) {
-				System.out.print(i+", ");
-			}
-		}
 		
+		System.out.println(index_list);
 		System.out.println("and the area is: "+list.get(min_index).getArea());
 		
 		System.out.println();
 		System.out.println("$$$$$$ CIRCLE LIST SORTED BY RADIUS $$$$$$");
 		for(int i=0; i<list.size(); i++)
-			System.out.println("Id: "+i+" - Radius: "+list.get(i).radius+" - Area: "+list.get(i).getArea());		
+			System.out.println("Id: "+i+" - Radius: "+list.get(i).radius+" - Area: "+list.get(i).getArea());	
 	}
 }
